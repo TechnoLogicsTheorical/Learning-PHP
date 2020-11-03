64cef49df986ee20bed45ecc51c3d18f00c83605
The Creative DEV <thecreativedev@outlook.fr>
2020-11-03 16:43:11 +0100

    Repair my files deleted ...

--- Les_Variables/constantes.php
+++
@@ -1,9 +0,0 @@
-<?php
-/* 
-    Nous découvrir l'utilisation des variables de types constantes qui va nous permettre de stocker
-    des valeurs, mais attention il y a une specifité à cette dernière que nous allons voir au fil
-    de cette section.
-
-    Les constantes stockent des valeurs qui ne peuvent pas etre modifiée au fil du programme dans 
-    notre cas cela est parfait
-*/
